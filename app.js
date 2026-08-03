d0fc1a678b9ba8f343e4e4b25bff632c6343ac6a
M-Moiz <mmuizsiddiqui@gmail.com>
2026-08-03 20:01:37 +0500

    working on js concepts

---
+++ app.js
@@ -0,0 +1,16 @@
+// var something = "spiderman";
+
+// console.log(something)
+
+var num1 = 5;
+
+function sum(){
+    // var num1 = 4;
+    // return num1;
+
+    console.log(num1)
+}
+
+sum();
+// console.log(num1)
+// console.log(sum())
